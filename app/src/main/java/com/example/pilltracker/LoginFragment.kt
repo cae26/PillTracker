59daaa1e54c87a0482f0b7c344e72a54ef89b689
cae26 <cae26@njit.edu>
2023-04-04 17:51:39 -0400

    login feature

--- app/src/main/java/com/example/pilltracker/LoginFragment.kt
+++ app/src/main/java/com/example/pilltracker/LoginFragment.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.pilltracker.R
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import org.json.JSONObject
