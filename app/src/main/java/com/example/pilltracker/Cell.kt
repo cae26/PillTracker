31fb60c555a923cb0e03237ac4a94064c879ee6f
Temitayo <tayoilori24@gmail.com>
2023-04-10 18:59:41 -0400

    -Used Rapid API for Search Medicine

---
+++ app/src/main/java/com/example/pilltracker/Cell.kt
@@ -0,0 +1,10 @@
+package com.example.pilltracker
+
+class Cell(
+    var name: String,
+    var price: String,
+    var content: String,
+    var companyName: String
+) {
+
+}
--- app/src/main/java/com/example/pilltracker/MainActivity.kt
+++ app/src/main/java/com/example/pilltracker/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.example.pilltracker
 
+
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.RequestParams
-import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
-import okhttp3.*
-import okhttp3.RequestBody.Companion.toRequestBody
-import org.json.JSONObject
-import java.io.IOException
-import kotlin.reflect.typeOf
-import org.json.JSONException
 
 
 
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         PillTrackerAPI().run()
 
