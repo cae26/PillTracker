82d17a9c516c05f5423aac4956408ca236e4b37f
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2023-05-01 09:20:02 -0400

    created notification functionality added xml files for design and visual notificcation works

--- app/src/main/java/com/example/pilltracker/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/pilltracker/MyFirebaseMessagingService.kt
@@ -3,9 +3,12 @@ package com.example.pilltracker
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -18,26 +21,88 @@ val channelName = "com.example.pilltracker"
 //
 //    override fun onMessageReceived(remoteMessage: RemoteMessage) {
 //        if (remoteMessage.getNotification() != null) {
-//                generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
-//            }
+//            generateNotification(
+//                remoteMessage.notification!!.title!!,
+//                remoteMessage.notification!!.body!!
+//                        remoteMessage.notification!!.context!!
+//            )
 //        }
 //
-//    fun getRemoteView(title: String, message: String): RemoteViews {
+//    }
+////    fun getRemoteView(title: String, message: String): RemoteViews {
+////        val remoteView = RemoteViews("com.example.pilltracker",R.layout.notification)
+////        remoteView.setTextViewText(R.id.title,title)
+////        remoteView.setTextViewText(R.id.notificationContent,message)
+////        remoteView.setImageViewResource(R.id.appLogo,R.drawable.ic_stat_ic_notification)
+////        remoteView.Button(R.id.takenButton,R.layout.notification)
+////        remoteView.Button(R.id.notTakenButton,R.layout.notification)
+////        return remoteView
+////    }
+//
+//    fun getRemoteView(title: String, message: String,context: Context): RemoteViews {
 //        val remoteView = RemoteViews("com.example.pilltracker",R.layout.notification)
-//        remoteView.setTextViewText(R.id.title,title)
+//       remoteView.setTextViewText(R.id.title,title)
 //        remoteView.setTextViewText(R.id.notificationContent,message)
 //        remoteView.setImageViewResource(R.id.appLogo,R.drawable.ic_stat_ic_notification)
+//        // Set the click listeners for the buttons
+//        val takenIntent = Intent(context, NotificationActionReceiver::class.java).apply {
+//            action = "com.example.pilltracker.TAKEN"
+//        }
+//        val takenPendingIntent = PendingIntent.getBroadcast(context, 0, takenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+//        remoteView.setOnClickPendingIntent(R.id.takenButton, takenPendingIntent)
+//
+//        val notTakenIntent = Intent(context, NotificationActionReceiver::class.java).apply {
+//            action = "com.example.pilltracker.NOT_TAKEN"
+//        }
+//        val notTakenPendingIntent = PendingIntent.getBroadcast(context, 0, notTakenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+//        remoteView.setOnClickPendingIntent(R.id.notTakenButton, notTakenPendingIntent)
+//
 //        return remoteView
 //    }
-//    fun generateNotification(title: String, message: String) {
-//        // create an intent for the notification
-//        val intent = Intent(this, MainActivity::class.java).apply {
+//
+////    fun generateNotification(title: String, message: String,context: Context) {
+////        // create an intent for the notification
+////        val intent = Intent(this, MainActivity::class.java).apply {
+////            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+////        }
+////        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+////
+////        // create the notification
+////        var builder: NotificationCompat.Builder= NotificationCompat.Builder(applicationContext, channelId)
+////            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+////            .setContentTitle(title)
+////            .setContentText(message)
+////
+////            .setAutoCancel(true)
+////            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+////            .setOnlyAlertOnce(true)
+////            .setContentIntent(pendingIntent)
+////        builder = builder.setContent(getRemoteView(title,message,context))
+////
+////        // Register the channel with the system
+////        val notificationManager: NotificationManager =
+////            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+////        // Create notification channel for API 26 and above
+////        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+////            val importance = NotificationManager.IMPORTANCE_HIGH
+////            val notificationChannel = NotificationChannel(channelId, channelName, importance)
+////            notificationManager.createNotificationChannel(notificationChannel)
+////            }
+////        notificationManager.notify(Random().nextInt(1000),builder.build())
+//
+//
+//    fun generateNotification(title: String, message: String, context: Context) {
+//        // Create an intent for the notification
+//        val intent = Intent(context, MainActivity::class.java).apply {
 //            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
 //        }
-//        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+//        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 //
-//        // create the notification
-//        var builder: NotificationCompat.Builder= NotificationCompat.Builder(applicationContext, channelId)
+//        // Create the custom RemoteViews for the notification
+//        val remoteViews = getRemoteView(message, title, context)
+//
+//        // Create the notification
+//        val builder = NotificationCompat.Builder(context, channelId)
 //            .setSmallIcon(R.drawable.ic_stat_ic_notification)
 //            .setContentTitle(title)
 //            .setContentText(message)
@@ -45,92 +110,126 @@ val channelName = "com.example.pilltracker"
 //            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
 //            .setOnlyAlertOnce(true)
 //            .setContentIntent(pendingIntent)
-//        builder = builder.setContent(getRemoteView(title,message))
+//            .setCustomContentView(remoteViews) // Set the custom RemoteViews for the notification
 //
-//        // Register the channel with the system
+//        // Register the notification channel with the system
 //        val notificationManager: NotificationManager =
-//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//        // Create notification channel for API 26 and above
+//            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 //            val importance = NotificationManager.IMPORTANCE_HIGH
 //            val notificationChannel = NotificationChannel(channelId, channelName, importance)
 //            notificationManager.createNotificationChannel(notificationChannel)
-//            }
-//        notificationManager.notify(Random().nextInt(1000),builder.build())
+//        }
 //
-//
-//
-//
-//
+//        // Show the notification
+//        notificationManager.notify(Random().nextInt(1000), builder.build())
 //    }
 //
-//    }
-
-
+//
+//
+//
+//}
+//
+//
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        if (remoteMessage.getNotification() != null) {
+        if (remoteMessage.notification != null) {
             generateNotification(
                 remoteMessage.notification!!.title!!,
-                remoteMessage.notification!!.body!!
+                remoteMessage.notification!!.body!!,
+                "",
+                applicationContext
             )
         }
     }
 
-    fun generateNotification(title: String, message: String) {
-        // create an intent for the notification
-        val intent = Intent(this, LogFragment::class.java).apply {
+    fun getRemoteView(title: String, message: String, user: String, context: Context): RemoteViews {
+        val remoteView = RemoteViews(context.packageName, R.layout.notification)
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.notificationContent, message)
+        remoteView.setImageViewResource(R.id.appLogo, R.drawable.ic_stat_ic_notification)
+
+        // Set the click listeners for the buttons
+        val takenIntent = Intent(context, NotificationActionReceiver::class.java).apply {
+            action = "com.example.pilltracker.TAKEN"
+            putExtra("title", title)
+            putExtra("message", message)
+            putExtra("user", user)
+        }
+        val takenPendingIntent = PendingIntent.getBroadcast(
+            context,
+            1,
+            takenIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        remoteView.setOnClickPendingIntent(R.id.takenButton, takenPendingIntent)
+
+        val notTakenIntent = Intent(context, NotificationActionReceiver::class.java).apply {
+            action = "com.example.pilltracker.NOT_TAKEN"
+            putExtra("title", title)
+            putExtra("message", message)
+            putExtra("user", user)
+        }
+        val notTakenPendingIntent = PendingIntent.getBroadcast(
+            context,
+            2,
+            notTakenIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        remoteView.setOnClickPendingIntent(R.id.notTakenButton, notTakenPendingIntent)
+
+        return remoteView
+    }
+
+
+    fun generateNotification(title: String, message: String, user: String, context: Context) {
+        // Create an intent for the notification
+        val intent = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         }
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
 
-        // create the intent for the "Taken" button click
-        val takenIntent = Intent(this, NotificationActionReceiver::class.java).apply {
-            action = "taken"
-        }
-        val takenPendingIntent =
-            PendingIntent.getBroadcast(this, 0, takenIntent, PendingIntent.FLAG_ONE_SHOT)
+        // Create the custom RemoteViews for the notification
+        val remoteViews = getRemoteView(title, message, user, context)
 
-        // create the intent for the "Not Taken" button click
-        val notTakenIntent = Intent(this, NotificationActionReceiver::class.java).apply {
-            action = "not_taken"
-        }
-        val notTakenPendingIntent =
-            PendingIntent.getBroadcast(this, 0, notTakenIntent, PendingIntent.FLAG_ONE_SHOT)
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_stat_ic_notification)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .setCustomContentView(remoteViews) // Set the custom RemoteViews for the notification
+            .setCustomBigContentView(remoteViews) // Set the custom RemoteViews for the expanded notification
 
-        // create the notification
-        var builder: NotificationCompat.Builder =
-            NotificationCompat.Builder(applicationContext, channelId)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
-                .setOnlyAlertOnce(true)
-                .setContentIntent(
-                    PendingIntent.getActivity(
-                        this,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_ONE_SHOT
-                    )
-                )
-                .addAction(R.drawable.ic_check_circle_outline, "Taken", takenPendingIntent)
-                .addAction(R.drawable.ic_cancel_outline, "Not Taken", notTakenPendingIntent)
-
-        // Register the channel with the system
+        // Register the notification channel with the system
         val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // Create notification channel for API 26 and above
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
             val notificationChannel = NotificationChannel(channelId, channelName, importance)
             notificationManager.createNotificationChannel(notificationChannel)
         }
+
+        // Show the notification
         notificationManager.notify(Random().nextInt(1000), builder.build())
     }
+
 }
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/pilltracker/NotificationActionReceiver.kt
+++ app/src/main/java/com/example/pilltracker/NotificationActionReceiver.kt
@@ -1,77 +1,129 @@
 package com.example.pilltracker
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.icu.text.SimpleDateFormat
-import android.os.AsyncTask
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.lang.StringBuilder
-import java.net.HttpURLConnection
-import java.net.URL
+import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONObject
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.random.Random
 
 class NotificationActionReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        // Get the action from the intent
         val action = intent.action
-        if (action == "taken" || action == "not_taken") {
-            val userName = "John Doe" // replace with actual user name
-            val medicineName = "Medicine A" // replace with actual medicine name
-            val status = if (action == "taken") "taken" else "not taken"
-            val dateTaken = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
 
-            val url = "https://group8.dhruvaldhameliya.com/insert_log.php" // replace with your actual API URL
-            val data = "user=$userName&medicine_name=$medicineName&status=$status&date_taken=$dateTaken"
-            println("action")
-            SendHttpPostRequest().execute(url, data)
+        // Check the action and handle it accordingly
+        when (action) {
+            "com.example.pilltracker.TAKEN" -> {
+                val title = intent.getStringExtra("title")
+                val message = intent.getStringExtra("message")
+
+                // Get the username from the SharedPreferences
+                val sharedPreferences = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+                val user = sharedPreferences.getString("username", "")
+                val taken = "Taken"
+
+                // Call the insertLog function to insert a log for the taken action
+                insertLog(context, taken, title!!, message!!, user!!)
+
+                // Dismiss the notification
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.cancelAll()
+            }
+
+            "com.example.pilltracker.NOT_TAKEN" -> {
+                val title = intent.getStringExtra("title")
+                val message = intent.getStringExtra("message")
+
+                // Get the username from the SharedPreferences
+                val sharedPreferences = context.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+                val user = sharedPreferences.getString("username", "")
+                val taken = "Not Taken"
+
+                // Call the insertLog function to insert a log for the not taken action
+                insertLog(context, taken, title!!, message!!, user!!)
+
+                // Dismiss the notification
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.cancelAll()
+            }
         }
     }
 
-    private inner class SendHttpPostRequest : AsyncTask<String, Void, String>() {
-        override fun doInBackground(vararg params: String?): String {
-            val urlString = params[0]
-            val postData = params[1]
+    fun date(): String {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
 
-            var result = ""
+    private fun insertLog(context: Context, taken: String, title: String, message: String, user: String) {
+        val url = "https://group8.dhruvaldhameliya.com/insert_log.php"
 
-            try {
-                val url = URL(urlString)
-                val conn = url.openConnection() as HttpURLConnection
-                conn.requestMethod = "POST"
-                conn.doOutput = true
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        val client = OkHttpClient()
 
-                val outputStreamWriter = OutputStreamWriter(conn.outputStream)
-                outputStreamWriter.write(postData)
-                outputStreamWriter.flush()
-                outputStreamWriter.close()
+        val requestBody = JSONObject().apply {
+            put("userName", user)
+            put("medicineName", message)
+            put("status", taken)
+            put("dateTaken", date())
+        }
 
-                val inputStreamReader = InputStreamReader(conn.inputStream)
-                val bufferedReader = BufferedReader(inputStreamReader)
-                val stringBuilder = StringBuilder()
-                var line: String?
-                while (bufferedReader.readLine().also { line = it } != null) {
-                    stringBuilder.append(line)
+        val request = Request.Builder()
+            .url(url)
+            .post(
+                RequestBody.create(
+                    "application/json".toMediaTypeOrNull(),
+                    requestBody.toString()
+                )
+            )
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body?.string()
+                    val jsonResponse = JSONObject(responseBody)
+
+                    // Handle the response here
+                } else {
+                    // Handle the error here
+                    Log.e("Error", "HTTP error code: ${response.code}")
+                    Handler(Looper.getMainLooper()).post {
+                        Toast.makeText(
+                            context,
+                            "Error: Failed to insert log",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
-                bufferedReader.close()
-                result = stringBuilder.toString()
-            } catch (e: Exception) {
-                e.printStackTrace()
             }
 
-            return result
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            if (result != null) {
-                Log.d("NotificationAction", "API response: $result")
-            } else {
-                Log.e("NotificationAction", "Error: API response is null")
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle the error here
+                Log.e("Error", "Failed to fetch data: ${e.message}")
+                Handler(Looper.getMainLooper()).post {
+                    Toast.makeText(
+                        context,
+                        "Error: ${e.localizedMessage}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
-        }
+        })
     }
+
 }
